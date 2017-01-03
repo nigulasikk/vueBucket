b2117f2c37a9268665741f91368ab201b442b0fa
qiankaijie <qiankaijie1024@gmail.com>
2017-01-03 15:05:45 +0800

    vue-router 动效

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <br>
-    <router-link to="/page1">Page1</router-link>
-    <router-link to="/page2">Page2</router-link>
-    <router-view></router-view>
+    <el-row class="tac">
+      <el-col :span="4">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <router-link to="/page1">
+            <el-menu-item index="2"><i class="el-icon-menu"></i>Page1</el-menu-item>
+          </router-link>
+          <router-link to="/page2">
+           <el-menu-item index="3"><i class="el-icon-setting"></i> Page2</el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-col>
+      <el-col :span="20">
+       <transition name="fade">
+        <router-view></router-view>
+       </transition>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -15,6 +42,14 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
@@ -27,5 +62,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a {text-decoration: none;color:none;}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
 }
 </style>
